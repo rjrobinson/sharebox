ac82ae90b2eb2ec5e63c42b21b04b0661202444b
RJ Robinson <robertj.robinson@gmail.com>
2014-11-07 16:22:01 -0500

    added tests

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :user do
-    
+    email 'sample@sample.com'
+    password '123456789'
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it 'can sign up' do
+    user = FactoryGirl.create(:user)
+
+    visit 'new_user_registration'
+    fill_in email, with: user.email
+    fill_in password, with: user.password
+
+    click_on 'Create user'
+    expect(page).to have_content('User created')
+  end
 end
